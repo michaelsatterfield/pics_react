11cc95bf437d613b80907e32db0d36c08da1fb60
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-02-13 15:54:44 -0600

    practicing map function to list items with react

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 const ImageList = (props) => {
-    console.log(props.images)
+   const images = props.images.map(({id,urls,description}) =>{
+    return <img key={id}  src={urls.small} alt={description}/>
+   });
+
     return (
-        <div>
-          ImageList
-        </div>
+        <div>{images}</div>
     );
-};
+}
 
 export default ImageList;
