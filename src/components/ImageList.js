2a33fad79ad2201a7b640826e159f1db3ea624f6
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-02-20 11:54:31 -0600

    finished splash api search application buit with react

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,11 @@
 import './ImageList.css'
 import React from 'react';
+import ImageCard from "./ImageCard";
+
 
 const ImageList = (props) => {
-   const images = props.images.map(({id,urls,description}) =>{
-    return <img key={id}  src={urls.small} alt={description}/>
+   const images = props.images.map((image) =>{
+    return <ImageCard key={image.id} image={image}/>
    });
 
     return (
